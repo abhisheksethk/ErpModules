b50a0594485a183c5f49fb53e433700a7105b951
abhishek seth <abhishekseth.3344@gmail.com>
2017-06-29 12:09:12 +0530

    EmpExpDAO

--- src/com/DAO/EmpExpDetailDAO.java
+++ src/com/DAO/EmpExpDetailDAO.java
@@ -1,5 +1,16 @@
 package com.DAO;
 
-public interface EmpExpDetailDAO {
+import java.util.List;
+
+import com.entity.EmpExpDetail;
+
+public interface EmpExpDetailDAO 
+{
+
+	public List<EmpExpDetail> getEmpExpDetails();
+
+	public void saveEmpExpDetail(EmpExpDetail theEmpExpDetail);
+
+	public EmpExpDetail getEmpExpDetail(int theId);
 
 }
